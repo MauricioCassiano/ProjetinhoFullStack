63ad5981c1f571a056fd29c67e0c20413d0b3c14
Mauricio cassiano de seles <mauriciocassiano@live.com>
2022-04-06 02:49:30 -0300

    Entendendo a estrutura HTTP

---
+++ src/Api/ProEventos.API/Model/Evento.cs
@@ -0,0 +1,19 @@
+namespace ProEventos.API.Model
+{
+    public class Evento
+    {
+        public int Id { get; set; }
+        public string Local { get; set; }
+        public DateTime? DataEvento { get; set; }
+        public string Tema { get; set; }
+        public int QtdPessoas { get; set; }
+        public string ImagemURL { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public IEnumerable<Lote> Lotes { get; set; }
+        public IEnumerable<RedeSocial> RedesSociais { get; set; }
+        public IEnumerable<PalestranteEvento> PalestrantesEventos { get; set; }
+    }
+}
